95b6678f587ecd24435c81ad547374037f2fc080
Carlos <cmalcazar@miners.utep.edu>
2021-10-03 21:03:50 -0600

    Changed makefile, tokenizer, and uimain.

--- src/uimain.c
+++ src/uimain.c
@@ -8,19 +8,27 @@ int main()
 {
 
   char userIn[100];
+  List *history = init_history();
   
-  while(1) {
-    putchar('$');
-    fgets(userIn, 100, stdin);
-    printf("%s\n", userIn);
-  }
+  printf("Please type a word to tokenize it. Or press 'q' to exit.");
 
-  space_char("s");
-  space_char(" ");
+  if (userIn[0] != 'q')
+    {
+      printf("Testing functions with: ", userIn);
+      int len = count_words(userIn);
+      printf("Number of words: ", len);
+      char* start = word_start(userIn);
+      printf("Word start is: ", start);
+      char* end = word_terminator(userIn);
+      printf("Word end is: ", end);
 
-  non_space_char("s");
-  non_space_char(" ");
-
+      // char * copy = copy_str(userIn, 
+      printf("Now tokenizing string");
+      char ** tokens = tokenize(userIn);
+      print_tokens(tokens);
+      printf("Now freeing tokens.");
+      free_tokens(tokens);
+      printf("History of tokenizer:");
+    }
   return 0;
-  
 }
